8361371dd0fae49dc79d13b03406e4b2e6d242b8
miraclew <miraclew@gmail.com>
2021-10-10 01:55:53 +0800

    try to fix windows crash issue

--- cgo-bindings/chiapos-cgo.h
+++ cgo-bindings/chiapos-cgo.h
@@ -6,34 +6,37 @@
 #define CHIAPOS_CHIAPOS_CGO_H
 
 #include "stdint.h"
+
+#define API __declspec(dllexport)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
     /* Prover */
     typedef void* Prover;
-    Prover ProverNew(const char* filePath);
-    void ProverFree(Prover p);
-    const char* ProverGetFileName(Prover p);
-    void ProverGetId(Prover p, uint8_t* buffer);
-    uint8_t ProverGetSize(Prover p);
-    uint32_t ProverGetMemoSize(Prover p);
-    void ProverGetMemo(Prover p, uint8_t* buffer);
+    API Prover ProverNew(const char* filePath);
+    API void ProverFree(Prover p);
+    API const char* ProverGetFileName(Prover p);
+    API void ProverGetId(Prover p, uint8_t* buffer);
+    API uint8_t ProverGetSize(Prover p);
+    API uint32_t ProverGetMemoSize(Prover p);
+    API void ProverGetMemo(Prover p, uint8_t* buffer);
     // Return list of quality string, which is sha256 each
-    void ProverGetQualitiesForChallenge(Prover p, uint8_t* challenge, uint8_t* buffer, uint32_t * count);
+    API void ProverGetQualitiesForChallenge(Prover p, uint8_t* challenge, uint8_t* buffer, uint32_t * count);
     // Return hex of 60*k bit string
-    const uint8_t* ProverGetFullProof(Prover p, const uint8_t* challenge, uint32_t index);
+    API const uint8_t* ProverGetFullProof(Prover p, const uint8_t* challenge, uint32_t index);
 
     /* Verifier */
     typedef void* VerifierGo;
-    VerifierGo VerifierGoNew();
-    void VerifierGoFree(VerifierGo p);
-    const uint8_t* VerifierValidateProof(VerifierGo verifier, const uint8_t* id, uint8_t k,
+    API VerifierGo VerifierGoNew();
+    API void VerifierGoFree(VerifierGo p);
+    API const uint8_t* VerifierValidateProof(VerifierGo verifier, const uint8_t* id, uint8_t k,
                                          const uint8_t* challenge,const uint8_t* proof_bytes,
                                          uint16_t proof_size);
 
     /* Debug */
-    void ThrowException(int8_t ec);
-    void CatchException(int8_t ec);
+    API void ThrowException(int8_t ec);
+    API void CatchException(int8_t ec);
 #ifdef __cplusplus
 }
 #endif
